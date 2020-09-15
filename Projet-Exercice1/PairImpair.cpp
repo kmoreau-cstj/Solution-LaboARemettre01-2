279d23534c2f01445fccebd415bb872fe087bb3f
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-14 22:31:43 -0400

    WOW !
    Quel programme complet et TRÈS, Très bien commenté. Excellent plan de tests !

--- Projet-Exercice1/PairImpair.cpp
+++ Projet-Exercice1/PairImpair.cpp
@@ -1,45 +1,45 @@
-// But : Écrire un programme qui lit un entier, qui le détermine et qui l'affiche à l'écran s'il s'agit d'un entier pair ou impair. 
-// Auteur : Noémie Lanctôt
+// But : Ã‰crire un programme qui lit un entier, qui le dÃ©termine et qui l'affiche Ã  l'Ã©cran s'il s'agit d'un entier pair ou impair. 
+// Auteur : NoÃ©mie LanctÃ´t
 // Date : 12/09/2020
 
-#include <iostream>		//Inclus le coffre à outils pour communiquer avec l'utilisateur (cout, cin, etc.) 
-// #include est la directive pour inclure les outils de la bibliothèque dans notre programme.
+#include <iostream>		//Inclus le coffre Ã  outils pour communiquer avec l'utilisateur (cout, cin, etc.) 
+// #include est la directive pour inclure les outils de la bibliothÃ¨que dans notre programme.
 
-/* Pour commencer un programme il faut créer une première fonction qui porte toujours le même nom : main pour principal - porte d'entrée de la fonction
-	Une fonction à 3 parties :
-	1. Son nom : Ne dois pas commencer par un chiffre et dois avoir une lettre minuscule au commencement. On met les majuscules à la place des espaces et aucun accent
-	2. Des parenthèses obligatoires pour les paramètres
-	3. Le titre de la valeur de retour - 2 possibilités pour le main
+/* Pour commencer un programme il faut crÃ©er une premiÃ¨re fonction qui porte toujours le mÃªme nom : main pour principal - porte d'entrÃ©e de la fonction
+	Une fonction Ã  3 parties :
+	1. Son nom : Ne dois pas commencer par un chiffre et dois avoir une lettre minuscule au commencement. On met les majuscules Ã  la place des espaces et aucun accent
+	2. Des parenthÃ¨ses obligatoires pour les paramÃ¨tres
+	3. Le titre de la valeur de retour - 2 possibilitÃ©s pour le main
 		- Void : Vide/rien, on ne retourne rien
-		- Int : Valeur entière (integer) et dans ce cas, main retourne quelque chose comme return 0; si tout va bien*/
+		- Int : Valeur entiÃ¨re (integer) et dans ce cas, main retourne quelque chose comme return 0; si tout va bien*/
 
-void main()		// Dans mon programme --> Void puisque ma fonction ne retournera rien		Main : Fonction principale/porte d’entrée 
+void main()		// Dans mon programme --> Void puisque ma fonction ne retournera rien		Main : Fonction principale/porte dâ€™entrÃ©e 
 {				// Cette accolade ouvre mon bloc d'instruction.
 
-	setlocale(LC_ALL, "");		// Convertir correctement les accents de Windows à la console
+	setlocale(LC_ALL, "");		// Convertir correctement les accents de Windows Ã  la console
 								// Chaque instruction dans mon programme doit se terminer par un ; 
 	
 
-	//Au début du programme on déclare toujours nos variables. Je déclare que ma variable est un entier 
-	int entier;			//Entier = int. Si j'avais eu une chaîne de texte comme variable, j'aurais choisi string au lieu de int 
+	//Au dÃ©but du programme on dÃ©clare toujours nos variables. Je dÃ©clare que ma variable est un entier 
+	int entier;			//Entier = int. Si j'avais eu une chaÃ®ne de texte comme variable, j'aurais choisi string au lieu de int 
 
-	//Pour commencer, on invite l'utilisateur à entrer un entier
-	std::cout << " Veuillez entrer un nombre entier : ";	// std::cout indique que la chaine de caractères entre guillemets doit être affichée à l'écran 
-															// << les becs de canards sont des caractères de redirection, texte du côté large des becs est redirigé vers l'écran out
+	//Pour commencer, on invite l'utilisateur Ã  entrer un entier
+	std::cout << " Veuillez entrer un nombre entier : ";	// std::cout indique que la chaine de caractÃ¨res entre guillemets doit Ãªtre affichÃ©e Ã  l'Ã©cran 
+															// << les becs de canards sont des caractÃ¨res de redirection, texte du cÃ´tÃ© large des becs est redirigÃ© vers l'Ã©cran out
 
-	//Le programme doit ensuite lire l'entier fournis par l'utilisateur pour savoir si c'est un entier pair ou impair 
+	//FR : Le programme doit ensuite lire l'entier fournis par l'utilisateur pour savoir si c'est un entier pair ou impair 
 	std::cin >> entier;
 
-	//Le programme détermine si l'entier est pair ou impair
-	if (entier%2 ==0)		// Si notre entier est un multiple de 2 et qu'il n'ya pas de reste, le nombre sera pair. Ma condition est toujours entre parenthèse suivi du if
+	//Le programme dÃ©termine si l'entier est pair ou impair
+	if (entier%2 ==0)		// Si notre entier est un multiple de 2 et qu'il n'ya pas de reste, le nombre sera pair. FR : Ma condition est toujours entre parenthÃ¨se suivi du if
 	{
-		std::cout << entier << " est un nombre pair ";			/*Tout texte doit être mis entre "guillemets" et ici, puisque l'entier rempli la conditiom, le programme affiche 
-																à l'écran que l'entier est pair*/
+		std::cout << entier << " est un nombre pair ";			/*Tout texte doit Ãªtre mis entre "guillemets" et ici, FR : puisque l'entier rempli la conditiom, le programme affiche 
+																Ã  l'Ã©cran que l'entier est pair*/
 	}
 
-	else			//Si l'entier ne rempli la condition, il sera un nombre impair. Pas besoin de répéter la condition puisqu'elle est déjà citée plus haut. 
+	else			//FR : Si l'entier ne rempli la condition, il sera un nombre impair. Pas besoin de rÃ©pÃ©ter la condition puisqu'elle est dÃ©jÃ  citÃ©e plus haut. 
 	{
-		std::cout << entier << " est un nombre impair ";		//Puisque l'entier ne rempli pas la condition, le programma affiche à l'écran que l'entier est impair
+		std::cout << entier << " est un nombre impair ";		// FR : Puisque l'entier ne rempli pas la condition, le programma affiche Ã  l'Ã©cran que l'entier est impair
 	}
 
 }	// Cette accolade ferme le bloc d'instruction de mon programme 
@@ -48,7 +48,7 @@ void main()		// Dans mon programme --> Void puisque ma fonction ne retournera ri
 PLAN DE TEST 
 
 				Entier									Pair OU impair
-	(Entier entré par l'utilisateur)	(Message que mon programme affiche à l'écran)
+	(Entier entrÃ© par l'utilisateur)	(Message que mon programme affiche Ã  l'Ã©cran)
 					0								0 est un nombre pair
 					-6								-6 est un nombre pair
 					-25								-25 est un nombre impair
